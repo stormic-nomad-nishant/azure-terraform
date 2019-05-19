3f0f54f6325abedb1deb5bb8d300a7d0419e068f
Nishant Singh <nissingh@nissingh-mn1.linkedin.biz>
2019-05-19 23:32:28 +0530

    Adding Virtual machine

--- Modules/Services/Virtual_machine/variable.tf
+++ Modules/Services/Virtual_machine/variable.tf
@@ -1,11 +1,12 @@
-resource "azurerm_virtual_machine" "" {
-  location = ""
-  name = ""
-  network_interface_ids = []
-  resource_group_name = ""
-  vm_size = ""
-  "storage_os_disk" {
-    create_option = ""
-    name = ""
-  }
+variable "azure-dc" {
+  default = "eastus"
+}
+variable "resource-grp-name" {
+  default = "test"
+}
+variable "machine-name" {
+  default = "demo"
+}
+variable "machine-type" {
+  default = "Standard_DS1_v2"
 }
